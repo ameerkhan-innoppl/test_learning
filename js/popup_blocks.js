da93d27e26d1b61caaa5aa5a72b8fb08cade0697
AmeerKhan <ameerkhan7000@gmail.com>
2018-02-05 19:05:19 +0530

    files folder css

--- js/popup_blocks.js
+++ js/popup_blocks.js
@@ -8,8 +8,8 @@
   'use strict';
 
 	$(document).ready(function(){
-	  $("#block-bartik-search").wrap('<div class="modal"></div>');
-	  $("#block-bartik-search").addClass("modal-content");
+	  $("#block-bartik-search, #block-searchform").wrap('<div class="modal"></div>');
+	  $("#block-bartik-search, #block-searchform").addClass("modal-content");
 	});
 
 })(jQuery, Drupal);
